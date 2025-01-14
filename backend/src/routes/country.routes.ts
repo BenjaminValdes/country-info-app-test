ab3a24856fbffb2ea467e6961fef43b1ed6353fd
Benjamin Valdes <benjamin_valdes@hotmail.com>
2025-01-14 18:20:59 -0300

    fix: changed backend route

--- backend/src/routes/country.routes.ts
+++ backend/src/routes/country.routes.ts
@@ -4,7 +4,7 @@ import countryController from '../controllers/country.controller'
 const router = Router();
 
 router.get('/countries', countryController.getAllCountries);
-router.get('/borderCountries/:id', countryController.getBorderCountries)
+router.get('/borderCountries/:code', countryController.getBorderCountries)
 router.get('/populationData', countryController.getBorderCountries)
 router.get('/countryFlags', countryController.getCountryFlags)
 
