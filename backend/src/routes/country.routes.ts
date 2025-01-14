e57c46d043bb6e0a152f1ba866b01c076ff37ae4
Benjamin Valdes <benjamin_valdes@hotmail.com>
2025-01-14 20:01:20 -0300

    fix in country routes

--- backend/src/routes/country.routes.ts
+++ backend/src/routes/country.routes.ts
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/countries', countryController.getAllCountries);
 router.get('/borderCountries/:code', countryController.getBorderCountries)
-router.get('/populationData', countryController.getBorderCountries)
+router.get('/populationData', countryController.getPopulationData)
 router.get('/countryFlags', countryController.getCountryFlags)
 
 export default router
